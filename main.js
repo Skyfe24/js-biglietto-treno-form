76a8358ea1e8fec75f02d0717090a57bc11dc92f
Skyfe24 <fabio.erase@gmail.com>
2023-05-29 16:38:39 +0200

    .toFixed(2) and .Values added, now calculations work good

--- main.js
+++ main.js
@@ -2,33 +2,30 @@ console.log('JS OK');
 //const total = document.getElementById('ticketPrice')
 
 
-const elementButton = document.getElementById('buttonX');
-const elementName = document.getElementById('Name');
-const elementKms = document.getElementById('kms');
-const elementAge = document.getElementById('age');
+const elementButton = document.getElementById('buttonX').value;
+const elementName = document.getElementById('Name').value;
+const elementKms = document.getElementById('kms').value;
+const elementAge = document.getElementById('age').value;
 const pricePerKm = 0.21;
 const discountUnder18 = 0.8;
 const discountOver65 = 0.6;
 
 const under18 = 18;
 const over65 = 65;
-console.log(elementName, elementKms, elementAge, pricePerKm, under18, over65, discountUnder18, discountOver65, buttonX);
+console.log(total, elementName, elementKms, elementAge, pricePerKm, under18, over65, discountUnder18, discountOver65, buttonX);
 
 
-let ticketPrice = elementKms * pricePerKm; // esce NaN in console.log e non so perchè
+let ticketPrice = elementKms * pricePerKm.toFixed(2); 
 console.log(ticketPrice);
 
 
 //proviamo a calcolare in base agli age-span forniti:
-if (elementAge === "Junior") {
-    ticketPrice = ticketPrice * discountUnder18
-} else if (elementAge === "Senior") {
-    ticketPrice = ticketPrice * discountOver65
-
-}
-
-
-
+if (elementAge === "Junior") 
+    ticketPrice = ticketPrice * discountUnder18;
+ else if (elementAge === "Senior") 
+    ticketPrice = ticketPrice * discountOver65;
+console.log(ticketPrice.toFixed(2));   
+// esce NaN in console.log e non so perchè
 
 
 //buttonX.addEventListener('click', function()) { da qui dovrà comparire una table col resoconto di tutto
